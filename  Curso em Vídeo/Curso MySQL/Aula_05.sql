492486f39da55d9bffb09d487ed18a86bd96f460
Lucas M. Gonçalves <lucas.66matheus@gmail.com>
2021-04-10 22:14:02 -0300

    Inserindo Dados na Tabela (INSERT INTO)

---
+++  Curso em Vídeo/Curso MySQL/Aula_05.sql
@@ -0,0 +1,11 @@
+INSERT INTO pessoas(nome, nascimento, sexo, peso, altura, nacionalidade) VALUES
+('Godofredo', '1984-01-02', 'M', '78.50', '1.83', 'Brasil'),
+('Maria', '1999-12-30', 'F', '55.20', '1.65', 'Portugal'),
+('Creuza', '1920-12-30', 'F', '50.20', '1.65', DEFAULT),
+('Adalgiza', '1930-11-02', 'F', '63.20', '1.75', 'Irlanda'),
+('Ana', '1975-12-22', 'F', '52.30', '1.45', 'Estados Unidos'),
+('Pedro', '2000-07-15', 'M', '52.30', '1.45', 'Brasil'),
+('Maria', '1999-05-30', 'F', '75.90', '1.70', 'Portugal'),
+('Cláudio', '1975-04-22', 'M', '99.00', '2.15', 'Brasil'),
+('Pedro', '1999-12-03', 'M', '87.00', '2.00', DEFAULT),
+('Janaina', '1987-11-12', 'F', '75.40', '1.66', 'Estados Unidos');
