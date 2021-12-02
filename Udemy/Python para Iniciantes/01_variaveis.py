66a9f44d17feeedda32bac8bef2963278093f81c
lucasmgon <lucas.66matheus@gmail.com>
2021-12-01 23:59:32 -0300

    Variáveis

---
+++ Udemy/Python para Iniciantes/01_variaveis.py
@@ -0,0 +1,13 @@
+valorhora = 4
+dias = 30
+horastrabalho = 8
+
+vencimentomensal = valorhora * horastrabalho*dias
+
+nome = "Lucas"
+
+print("Holerite de " + nome)
+print(valorhora)
+print(dias)
+print(horastrabalho)
+print(vencimentomensal)
