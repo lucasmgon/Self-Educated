a2e7d3d23910aeff209328116669fda5783ed310
lucasmgon <lucas.66matheus@gmail.com>
2022-01-03 00:52:56 -0300

    Tabuada.

---
+++ Prepara Cursos/Pascal/Aula 03/02_tabuada.pas
@@ -0,0 +1,19 @@
+//Tabuada.
+Program tabuada;
+var numero: real;
+
+begin
+	write('Digite um numero: ');
+	read(numero);
+	writeln('01.00 X ',numero:2:2,' = ',numero*1 :2:2);
+	writeln('02.00 X ',numero:2:2,' = ',numero*2 :2:2);
+	writeln('03.00 X ',numero:2:2,' = ',numero*3 :2:2);
+	writeln('04.00 X ',numero:2:2,' = ',numero*4 :2:2);
+	writeln('05.00 X ',numero:2:2,' = ',numero*5 :2:2);
+	writeln('06.00 X ',numero:2:2,' = ',numero*6 :2:2);
+	writeln('07.00 X ',numero:2:2,' = ',numero*7 :2:2);
+	writeln('08.00 X ',numero:2:2,' = ',numero*8 :2:2);
+	writeln('09.00 X ',numero:2:2,' = ',numero*9 :2:2);
+	writeln('10.00 X ',numero:2:2,' = ',numero*10 :2:2);
+	readln;
+end.
