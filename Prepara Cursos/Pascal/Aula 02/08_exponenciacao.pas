4f38d3dab46dfdf302ed0438fd26c3004195f8de
lucasmgon <lucas.66matheus@gmail.com>
2022-01-03 00:11:05 -0300

    Calcule a potência de 3 de um número qualquer.

---
+++ Prepara Cursos/Pascal/Aula 02/08_exponenciacao.pas
@@ -0,0 +1,12 @@
+//Calcule a potência de 3 de um número qualquer.
+Program potencia;
+var n, resultado;
+begin
+	write('Digite um numero: ');
+	read(n);
+
+	resultado exp(3*ln(n));
+
+	write('O resultado e: ', resultado);
+	readln;
+end.
