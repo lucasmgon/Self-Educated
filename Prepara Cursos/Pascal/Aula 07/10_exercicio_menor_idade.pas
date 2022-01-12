d2e60372ac12e2ad20bd3c1b3d1e2ae5fc91f548
Lucas Matheus Gonçalves <72113331+lucasmgon@users.noreply.github.com>
2022-01-12 15:22:13 -0300

    Faça um algoritmo que realize a soma das idades menores que 18 anos.

---
+++ Prepara Cursos/Pascal/Aula 07/10_exercicio_menor_idade.pas
@@ -0,0 +1,21 @@
+//Faça um algoritmo que receba o usuário digite uma quantidade de pessoas, e realize a soma das idades apenas das pessoas que tiverem idade menor que 18 anos.
+program maior_idade;
+var
+	cont, aux, soma, idade: integer;
+begin
+	write('Quantas idades deseja somar? ');
+	read(cont);
+
+	for aux:= 1 to cont do
+		begin
+			write('Digite a ',aux,'° idade: ');
+			read(idade);
+			
+			if idade < 18 then
+				begin
+					soma:= soma + idade;
+				end
+		end;
+	write('A soma das idades e: ', soma);
+	readln;
+end.
