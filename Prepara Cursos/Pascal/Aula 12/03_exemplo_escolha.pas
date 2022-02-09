c4986b76bcfc1a8ed1c0e1abd9004a33ee7bc8b0
lucasmgon <lucas.66matheus@gmail.com>
2022-02-09 00:06:16 -0300

    Código final.

--- Prepara Cursos/Pascal/Aula 12/03_exemplo_escolha.pas
+++ Prepara Cursos/Pascal/Aula 12/03_exemplo_escolha.pas
@@ -20,7 +20,25 @@ begin
 	read(n1);
 	write('Digite o segundo numero: ');
 	read(n2);
-	subtracao:= n1 + n2;
-	write('A subtracao e: ', subtracao);
+	subtracao:= n1 - n2;
+	writeln('A subtracao e: ', subtracao);
 	readln;
 end;
+
+var opcao: integer;
+
+begin
+	writeln('Escolha uma opcao');
+	writeln('1 - Soma');
+	write('2 - Subtração: ');
+	read(opcao);
+	
+	if opcao = 1 then
+		begin
+			soma;
+		end
+	else
+		begin
+			subtracao;
+		end;
+end.
