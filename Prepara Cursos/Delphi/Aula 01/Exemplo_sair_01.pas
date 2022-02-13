64442b395eee48836085cb5844c12aef727b65b8
lucasmgon <lucas.66matheus@gmail.com>
2022-02-13 16:21:32 -0300

    Button sair.

---
+++ Prepara Cursos/Delphi/Aula 01/Exemplo_sair_01.pas
@@ -0,0 +1,31 @@
+unit Exemplo_sair_01;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, StdCtrls;
+
+type
+  TForm1 = class(TForm)
+    Button1: TButton;
+    procedure Button1Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  application.Terminate;
+end;
+
+end.
