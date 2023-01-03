2d020e668e509352ca648565c5366121d86dc6ee
Lucas Matheus Gonçalves <72113331+lucasmgon@users.noreply.github.com>
2023-01-02 23:39:09 -0300

    C++

---
+++ Udemy/C++/04_caracteres.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	char c = 'a';
+	char d = 'b';
+
+	cout << c;
+	cout << '\n';
+	cout << (int)c;
+	cout << '\n';
+	cout << c + d;
+	return 0;
+}
