6712abf0e0ccd645af894937ed93dd16b0c44b96
Lucas Matheus Gonçalves <72113331+lucasmgon@users.noreply.github.com>
2023-01-03 23:58:07 -0300

    C++

---
+++ Udemy/C++/05_condicao_if.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int dinheiro = 20;
+	
+	if(dinheiro > 10){
+		cout << "Vou ao cinema!";
+	}else{
+		cout << "Não vou ao cinema!";
+	}
+	return 0;
+}
