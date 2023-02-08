189939c783cad710213ccf3368224aac1099b1b3
lucasmgon <lucas.66matheus@gmail.com>
2023-02-08 01:58:31 -0300

    Melhoria no código.

--- Udemy/C++/Curso de C++ - Intermediário/25_classes.cpp
+++ Udemy/C++/Curso de C++ - Intermediário/25_classes.cpp
@@ -8,32 +8,35 @@ public:
 	int numero;
 	double saldo;
 	
-	// Função membro da Classe
-	double depositar(double quantidade){
-		if(quantidade > 0){
-			saldo += quantidade;
-		}
-		return saldo;
-	}
-	
-	double retirar(double quantidade){
-		if(quantidade > 0 && saldo >= quantidade){
-			saldo -= quantidade;
-		}
-		return saldo;
-	}
+	double depositar(double quantidade);
+	double retirar(double quantidade);	
 };
+
+double Conta::depositar(double quantidade){
+	if(quantidade > 0){
+		this->saldo += quantidade;
+	}
+	return this->saldo;
+}
+
+double Conta::retirar(double quantidade){
+	if(quantidade > 0 && saldo >= quantidade){
+		this->saldo -= quantidade;
+	}
+	return this->saldo;
+}
 
 int main(int argc, char *argv[]){
 	
 	Conta c;
+	Conta* pc = &c; // pc é um ponteiro do tipo Conta que guarda o endereço do objeto c
 	
-	c.numero = 1;
-	c.saldo = 100.75;
+	pc->numero = 1; // pc acessa número e guarda 1
+	pc->saldo = 200; // pc acessa número e guarda 200
 	
 	cout << "Saldo:" << c.saldo << endl;
-	cout << "Saldo depois do deposito: " << c.depositar(100) << endl;
-	cout << "Saldo depois do saque: " << c.retirar(50) << endl;
+	cout << "Saldo depois do deposito: " << pc->depositar(100) << endl;
+	cout << "Saldo depois do saque: " << pc->retirar(50) << endl;
 	
 	return 0;
 }
