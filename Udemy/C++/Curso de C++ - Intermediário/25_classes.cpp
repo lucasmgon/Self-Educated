627e3553bcf24ddf484cbc75b0834b27519e6f72
lucasmgon <lucas.66matheus@gmail.com>
2023-02-10 20:26:28 -0300

    Classes e Ponteiros.

--- Udemy/C++/Curso de C++ - Intermediário/25_classes.cpp
+++ Udemy/C++/Curso de C++ - Intermediário/25_classes.cpp
@@ -10,6 +10,14 @@ public:
 	
 	double depositar(double quantidade);
 	double retirar(double quantidade);	
+	
+	double getSaldo(){
+		return this->saldo;
+	}
+	
+	int getNumero(){
+		return this->numero;
+	}
 };
 
 double Conta::depositar(double quantidade){
@@ -26,17 +34,35 @@ double Conta::retirar(double quantidade){
 	return this->saldo;
 }
 
+void foo(Conta& c){
+	c.depositar(50);
+}
+
+Conta* novaConta(int numero){
+	Conta* c = new Conta;
+	c->numero = numero;
+	c->saldo = 0.0;
+	return c;
+}
+
 int main(int argc, char *argv[]){
 	
-	Conta c;
-	Conta* pc = &c; // pc é um ponteiro do tipo Conta que guarda o endereço do objeto c
+	//Conta c;
+	//Conta* pc = &c; // pc é um ponteiro do tipo Conta que guarda o endereço do objeto c
 	
-	pc->numero = 1; // pc acessa número e guarda 1
-	pc->saldo = 200; // pc acessa número e guarda 200
+	Conta* pc = novaConta(1111);
 	
-	cout << "Saldo:" << c.saldo << endl;
-	cout << "Saldo depois do deposito: " << pc->depositar(100) << endl;
-	cout << "Saldo depois do saque: " << pc->retirar(50) << endl;
+	//pc->numero = 1; // pc acessa número e guarda 1
+	//pc->saldo = 200; // pc acessa número e guarda 200
+	
+	//cout << "Saldo:" << c.saldo << endl;
+	//cout << "Saldo depois do deposito: " << pc->depositar(100) << endl;
+	//cout << "Saldo depois do saque: " << pc->retirar(50) << endl;
+	
+	//foo(c);
+	
+	cout << "Numero: " << pc->getNumero() << endl; 
+	cout << "Saldo: " << pc->getSaldo() << endl;
 	
 	return 0;
 }
