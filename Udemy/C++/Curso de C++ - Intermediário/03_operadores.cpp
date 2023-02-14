5bcba340578da4758b686ac6b1ccf3ef96ee341e
lucasmgon <lucas.66matheus@gmail.com>
2023-02-13 23:58:15 -0300

    Alterado.

--- Udemy/C++/Curso de C++ - Intermediário/03_operadores.cpp
+++ Udemy/C++/Curso de C++ - Intermediário/03_operadores.cpp
@@ -3,11 +3,11 @@
 using namespace std;
 
 int main(){
-	int idade = 20, idade2 = 20, resultado = 0;
+	int idade = 20, idade2 = 20;
 	
-	resultado = idade + idade2;
+	idade += idade2;
 	
-	cout << resultado;
+	cout << idade;
 	
 	return 0;
 }
