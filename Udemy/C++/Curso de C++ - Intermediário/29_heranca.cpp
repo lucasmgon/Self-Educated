9dba4cefbaa0e73d4b19d9a7aa37f912142c01d6
lucasmgon <lucas.66matheus@gmail.com>
2023-02-16 23:59:43 -0300

    Herança.

---
+++ Udemy/C++/Curso de C++ - Intermediário/29_heranca.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+class Animal{
+protected:
+	char* nome;
+
+public:
+	Animal(const char* nome){
+		cout << "Construindo animal..." << endl;;
+		nome = new char[strlen(nome) + 1];
+		strcpy(this->nome, nome);
+	}
+	
+	~Animal(){
+		delete[] nome;
+		nome = 0;
+	}
+	
+	const char* getNome(){
+		return nome;
+	}
+};
+
+class Cachorro : public Animal{
+protected:
+	int idade;
+
+public:
+	Cachorro(const char* nome) : Animal(nome){
+		cout << "Construindo cachorro..." << endl;
+		idade = 0;
+	}
+	
+	int getIdade(){
+		return idade;
+	}
+	
+	void setIdade(int idade){
+		this->idade = idade;
+	}
+};
+
+int main(int argc, char *argv[]){
+	
+	Cachorro c("Thor");
+	c.setIdade(5);
+	
+	cout << "Nome: " << c.getNome() << endl;
+	cout << "Idade: " << c.getIdade();
+	
+	return 0;
+}
