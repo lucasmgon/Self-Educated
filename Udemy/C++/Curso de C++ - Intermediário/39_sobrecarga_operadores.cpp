c1674a42bb2a490513ea19bdc67469bb92403a23
lucasmgon <lucas.66matheus@gmail.com>
2023-02-23 23:50:18 -0300

    Sobrecarga de operadores.

---
+++ Udemy/C++/Curso de C++ - Intermediário/39_sobrecarga_operadores.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+// Número complexo
+// n1 = 1 + 2i
+// n2 = 3 + d1
+// n3 = n1 + n2 = 4 + 6i
+
+class Complexo{
+public:
+	int real, imag;
+	
+	Complexo(int real, int imag){
+		this->real = real;
+		this->imag = imag;
+	}
+	
+	Complexo operator+(Complexo& c){
+		return Complexo(this->real + c.real, this->imag + c.imag);
+	}
+};
+
+int main(int argc, char *argv[]){
+	
+	Complexo c1(1, 2), c2(3, 4);
+	//Complexo c3 = c1 + c2;
+	Complexo c3 = c1.operator+(c2);
+	
+	cout << "Parte real: " << c3.real << endl;
+	cout << "Parte imaginaria: " << c3.imag << endl;
+		
+	return 0;
+}
