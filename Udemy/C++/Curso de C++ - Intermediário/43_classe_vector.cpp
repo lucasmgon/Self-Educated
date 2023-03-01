2cb7fdc14e4be987390933306d8a8dc7ec28964c
Lucas Matheus Gonçalves <72113331+lucasmgon@users.noreply.github.com>
2023-03-01 17:10:03 -0300

    Classe vector

---
+++ Udemy/C++/Curso de C++ - Intermediário/43_classe_vector.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char *argv[]){
+
+	vector<int> v(3);
+	v[0] = 10;
+	v[1] = 20;
+	v[2] = 30;
+
+	vector<int>::iterator it = v.begin();
+
+	for(it = v.begin(); it != v.end(); it++){
+		cout << *it << endl;
+	}
+
+	cout << "Primeiro elemento: " << *v.begin() << endl;
+	cout << "Ultimo elemento: " << *(--v.end()) << endl;
+
+	vector<int>::reverse_iterator rit;
+	int i = 0;
+
+	for(rit = v.rbegin(); rit != v.rend(); rit++){
+		*rit = ++i;
+	}
+
+	for(it = v.begin(); it != v.end(); it++){
+		cout << *it << endl;
+	}
+
+	return 0;
+}
