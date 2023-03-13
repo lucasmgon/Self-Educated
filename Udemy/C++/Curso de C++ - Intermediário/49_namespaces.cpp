d1dc0196dde562783b5a83d64b7ae8935a151fd7
Lucas Matheus Gonçalves <72113331+lucasmgon@users.noreply.github.com>
2023-03-12 21:09:41 -0300

    Namespaces

---
+++ Udemy/C++/Curso de C++ - Intermediário/49_namespaces.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+namespace ns1{
+	int num = 42;
+
+	class A{
+	public:
+		void imprimir(){
+			std::cout << "Ola, sou a classe A do namespace ns1\n";
+		}
+	};
+}
+
+namespace ns2{
+	int num = 50;
+
+	class A{
+	public:
+		void imprimir(){
+			std::cout << "Ola, sou a classe A do namespace ns2\n";
+		}
+	};
+}
+
+int main(int argc, char *argv[]){
+	std::cout << ns2::num << std::endl;
+
+	ns1::A ns1A;
+	ns2::A ns2A;
+
+	ns1A.imprimir();
+	ns2A.imprimir();
+
+	return 0;
+}
