05c806b80d48adfb890ba8a1ca3d5a795e379b01
Lucas Matheus Gonçalves <72113331+lucasmgon@users.noreply.github.com>
2023-05-27 16:46:28 -0300

    Update Consultas.sql
    Concatenação.

--- Udemy/SQL Server 2019 - do SQL a Administração/Consultas.sql
+++ Udemy/SQL Server 2019 - do SQL a Administração/Consultas.sql
@@ -12,3 +12,10 @@ SELECT nome, salario, salario-(salario*0.2) AS Desconto FROM funcionario;
 
 SELECT * FROM venda;
 SELECT produto_vend, quantidade, quantidade+50 AS "Estoque Futuro" FROM venda;
+
+/* Concatenação */
+SELECT nome, cargo FROM funcionario;
+SELECT nome + ' trabalha como: ' + cargo AS "Nome e função" FROM funcionario;
+SELECT nome + ' mora em ' + cidade AS 'Nome e cidade' FROM funcionario;
+
+SELECT CONCAT(nome, ' recebe o salário de: R$ ', salario) AS 'Nome e salário' FROM funcionario;
