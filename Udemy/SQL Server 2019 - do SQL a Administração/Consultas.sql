a1e3d357d32eced64af424ec56d91a6762dc2d1f
Lucas Matheus Gonçalves <72113331+lucasmgon@users.noreply.github.com>
2023-05-27 16:41:28 -0300

    Update Consultas.sql
    Funções aritméticas.

--- Udemy/SQL Server 2019 - do SQL a Administração/Consultas.sql
+++ Udemy/SQL Server 2019 - do SQL a Administração/Consultas.sql
@@ -3,3 +3,9 @@ SELECT nome FROM funcionario;
 SELECT nome, cidade FROM funcionario;
 SELECT * FROM funcionario;
 SELECT * FROM produto;
+
+/* Funções aritméticas na instrução SELECT */
+SELECT nome, salario FROM funcionario;
+SELECT nome, salario, salario+50 AS "Abono de férias" FROM funcionario;
+SELECT nome, salario, salario+(salario*0.2) AS Abono FROM funcionario;
+SELECT nome, salario, salario-(salario*0.2) AS Desconto FROM funcionario;
