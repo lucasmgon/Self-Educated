d8b34760c4571cbf1247ef4ced376b30bc1adecf
Lucas Matheus Gonçalves <72113331+lucasmgon@users.noreply.github.com>
2023-05-28 22:01:14 -0300

    Update Consultas.sql
    Intervalo de testes - BETWEEN.

--- Udemy/SQL Server 2019 - do SQL a Administração/Consultas.sql
+++ Udemy/SQL Server 2019 - do SQL a Administração/Consultas.sql
@@ -38,3 +38,12 @@ SELECT nome, nascimento, cargo FROM funcionario WHERE cargo='Vendedor';
 SELECT * FROM funcionario WHERE cidade='São Paulo';
 SELECT * FROM funcionario WHERE data_contratacao='2010-10-10';
 SELECT * FROM funcionario WHERE data_contratacao>='2010-10-10';
+
+/* Especificando um intervalo a ser testado - BETWEEN */
+SELECT * FROM funcionario;
+SELECT * FROM funcionario WHERE salario BETWEEN 1500 AND 2500;
+SELECT * FROM funcionario WHERE data_contratacao BETWEEN '2009-01-01' AND '2010-10-10';
+SELECT * FROM funcionario WHERE salario NOT BETWEEN 1500 AND 2500;
+
+SELECT * FROM produto;
+SELECT nome_prod, estoque FROM produto WHERE estoque BETWEEN 10 AND 50;
